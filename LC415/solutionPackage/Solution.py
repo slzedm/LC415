b8ee3c9380173921fca06e5adbd3b9994bb36951
nicomp42 <nicholdw@ucmail.uc.edu>
2022-11-17 07:49:13 -0500

    First Commit

---
+++ LC415/solutionPackage/Solution.py
@@ -0,0 +1,16 @@
+# LC415.py
+# Solution to the LeetCode problem 415
+# https://leetcode.com/problems/add-strings/
+class Solution:
+    def toInt(self, num):
+        answer = 0
+        for d in num:
+            answer = answer * 10 + (ord(d)-48) 
+        return answer
+        
+    def addStrings(self, num1: str, num2: str) -> str:
+        n1 = self.toInt(num1)
+        n2 = self.toInt(num2)
+        print(n1,n2)
+        answer = n1+n2
+        return str(answer)
